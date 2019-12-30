d073ff3ec368f7e7e5bf25019114c2b7fe870e32
BestOfQ <3434614710@qq.com>
2019-12-30 20:29:25 +0800

    网页跳转
    直接跳转到products中去

--- ph/urls.py
+++ ph/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# 导入products（产品）的views
+import products.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', products.views.home),
 ]
