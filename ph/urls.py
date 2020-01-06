15207c7c1a9521b14cd421a85f32a7e48c3bf795
BestOfQ <3434614710@qq.com>
2020-01-06 20:47:33 +0800

    初步完成发布产品app-1

--- ph/urls.py
+++ ph/urls.py
@@ -22,5 +22,7 @@ import products.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', products.views.product_list, name='主页'),
-    path('account/', include('account.urls')),    
+    path('account/', include('account.urls')),  
+    path('products/', include('products.urls')),    
+
 ]
